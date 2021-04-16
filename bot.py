f94f506ba0bfe595d4e0589ca458ed2b407b62c7
qxffo <81269560+qxffo@users.noreply.github.com>
2021-04-16 12:53:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -134,6 +134,8 @@ async def help(ctx):
     embed.set_author(name='Source Code')
     embed.add_field(name=';kick', value='Kicks mentioned user', inline=False)
     embed.add_field(name=';ban', value='Bans mentioned user', inline=False)
+    embed.add_field(name=';mute', value='Mutes mentioned user', inline=False)
+    embed.add_field(name=';unmute', value='Unmutes mentioned user', inline=False)
     embed.add_field(name=';clear', value='Deletes messages.', inline=False)
     embed.add_field(name=';changeprefix', value='Changes server prefix', inline=False)
     embed.add_field(name=';play', value='Plays a selected song from youtube', inline=False)
@@ -172,6 +174,56 @@ async def _8ball(ctx, *, question):
                  'Better not tell you now']
     await ctx.send(f'Question: {question}\nAsnwer: {random.choice(responses)}')
 #Mute Command
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member, *, reason=None):
+    guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
+
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
+
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+
+    await member.add_roles(mutedRole, reason=reason)
+    await ctx.send(f'Muted {member.mention} for reason {reason}')
+    await member.send(f'You were muted in the server {guild.name} for reason {reason}')
+#Mute Error
+@mute.error
+async def mute_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        
+        embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+
+        embed.set_author(name='You do not have permision to use this command!')
+
+        await ctx.send(embed=embed)
+#Unmute Command
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+    await member.remove_roles(mutedRole)
+    await ctx.send(f'Unmuted {member.mention}')
+    await member.send(f'You were unmuted in the server {guild.name}')
+#Unmute Error
+@unmute.error
+async def unmute_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        
+        embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+
+        embed.set_author(name='You do not have permision to use this command!')
+
+        await ctx.send(embed=embed)
+
+
 
 
 client.run('TOKEN')
