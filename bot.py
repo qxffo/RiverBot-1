085563a84bfb30c232169f11f8f6364db88574f0
qxffo <81269560+qxffo@users.noreply.github.com>
2021-04-27 12:10:23 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ client.add_cog(music_cog(client))
 @client.event
 async def on_ready():
   print('Logged into {0.user}'.format(client))
-  await client.change_presence(status=discord.Status.online, activity=discord.Game('PICKLE RICK!!! | ;help'))
+  await client.change_presence(status=discord.Status.online, activity=discord.Game('me when | ;help'))
 #Change Prefix
 @client.event
 async def on_guild_join(guild):
@@ -59,7 +59,7 @@ async def changeprefix(ctx, prefix):
         colour = discord.Colour.purple()
     )
 
-    embed.set_author(name='Prefix changed to: {prefix}')
+    embed.set_author(name='Prefix changed to: ' + prefix)
 
     await ctx.send(embed=embed)
 
@@ -125,6 +125,7 @@ async def ban_error(ctx, error):
 #Help Command
 @client.command(pass_context=True)
 async def help(ctx):
+
     author = ctx.message.channel
 
     embed = discord.Embed(
@@ -179,6 +180,7 @@ async def _8ball(ctx, *, question):
 async def mute(ctx, member: discord.Member, *, reason=None):
     guild = ctx.guild
     mutedRole = discord.utils.get(guild.roles, name="Muted")
+    uwuRole = discord.utils.get(guild.roles, name="uwu")
 
     if not mutedRole:
         mutedRole = await guild.create_role(name="Muted")
@@ -187,8 +189,11 @@ async def mute(ctx, member: discord.Member, *, reason=None):
             await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
 
     await member.add_roles(mutedRole, reason=reason)
+    await member.remove_roles(uwuRole)
+
+    
     await ctx.send(f'Muted {member.mention} for reason {reason}')
-    await member.send(f'You were muted in the server {guild.name} for reason {reason}')
+    await member.send(f'You were muted in the server **{guild.name}** For reason **{reason}**')
 #Mute Error
 @mute.error
 async def mute_error(ctx, error):
@@ -206,10 +211,12 @@ async def mute_error(ctx, error):
 @commands.has_permissions(manage_messages=True)
 async def unmute(ctx, member: discord.Member):
     mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+    uwuRole = discord.utils.get(ctx.guild.roles, name="uwu")
 
     await member.remove_roles(mutedRole)
+    await member.add_roles(uwuRole)
     await ctx.send(f'Unmuted {member.mention}')
-    await member.send(f'You were unmuted in the server {guild.name}')
+    await member.send(f'You were unmuted in ' + {guild.name})
 #Unmute Error
 @unmute.error
 async def unmute_error(ctx, error):
